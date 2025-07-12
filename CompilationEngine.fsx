9ea8db20297bc76d964a605cb4921479959108c8
Alex Wilson <alexrodwilson@gmail.com>
2025-07-12 16:04:59 +0100

    feat: added CompileSubroutineDecs

--- CompilationEngine.fsx
+++ CompilationEngine.fsx
@@ -18,6 +18,16 @@ let advanceUntil test tokens returnLastToken =
     | head::tail -> aux (head::toReturn) tail
   aux [] tokens
 
+let advanceUntilMatchingCurlyBracket tokens = 
+  let rec aux toReturn remainingTokens lbs rbs = 
+    match remainingTokens with
+    | [] -> failwith "No matching bracket found"
+    | head::tail when head = (Symbol '{') -> aux (head::toReturn) tail (lbs + 1) rbs
+    | head::tail when head = (Symbol '}') && (lbs = rbs + 1) -> List.rev (head::toReturn), tail
+    | head::tail when head = (Symbol '}') -> aux (head::toReturn) tail lbs (rbs + 1)
+    | head::tail -> aux (head::toReturn) tail lbs rbs
+  aux [] tokens 0 0
+
 
 let getConsecutivePatterns startTest endTest tokens =
   let rec aux patterns remainingTokens = 
@@ -32,12 +42,12 @@ let getConsecutivePatterns startTest endTest tokens =
 let eatIf (test: Token list -> bool) (tokens: Token list) =
   match tokens with
   | head::tail when (test tokens) -> tail
-  | _ -> failwith "Bad Argument" 
+  | head::tail -> failwith ("Bad Argument " + (string head)) 
 
 let getNextTokenIf test tokens = 
   match tokens with
   | head::tail when (test tokens) -> head, tail
-  | head::tail -> failwith "Bad Argument"
+  | head::tail -> failwith ("Bad Argument " + (string head)) 
 
 let isSameToken (token: Token) = 
   (fun (xs: Token list) -> xs.Head = token)
@@ -61,6 +71,9 @@ let isTypeProgramStructure xs =
   (isSameToken (Keyword "int") xs) ||
   (isSameToken (Keyword "char") xs)||
   (isSameToken (Keyword "boolean") xs)
+
+let isTypeOrVoid xs =
+  (isTypeProgramStructure xs) || xs.Head = (Keyword "void")
 
 let CompileClassVarDecs tokens = 
   let classVarDecPatterns, remainingTokens = getConsecutivePatterns (fun x -> x = (Keyword "static") || x = (Keyword "field")) (fun x -> x = (Symbol ';')) tokens
@@ -87,15 +100,56 @@ let CompileClassVarDecs tokens =
     (xml, remainingTokens)
 
 
-let CompileSubroutineDecs tokens =
-  ("", tokens)
+let CompileParameterList tokens =
+  "", tokens
 
+let CompileSubroutineBody (tokens: Token list) = 
+  $$"""<symbol> { </symbol>
+<symbol> } </symbol>
+"""
+
+
+
+let CompileSubroutineDecs tokens =
+  let doOneSubroutineDec tokens =
+    let constructorFunctionMethod, ts = getNextTokenIf (isOneOfTokens [Keyword "constructor"; Keyword "function"; Keyword "method"]) tokens
+    let voidOrType, ts = getNextTokenIf isTypeOrVoid ts
+    let subroutineName, ts = getNextTokenIf (isSameType (Identifier "_")) ts
+    let ts = eatIf (isSameToken (Symbol '(')) ts
+    let parameterTokens, ts = advanceUntil (fun x -> x = (Symbol ')')) ts false
+    let parameterXml, parameterCount = CompileParameterList parameterTokens 
+    let ts = eatIf (isSameToken (Symbol ')')) ts
+    let subroutineBodyTokens, ts = advanceUntilMatchingCurlyBracket ts
+    let subroutineBodyXml = CompileSubroutineBody subroutineBodyTokens
+    $$"""<subroutineDec>
+  {{tokenToXml constructorFunctionMethod}}
+  {{tokenToXml voidOrType}}
+  {{tokenToXml subroutineName}}
+  <symbol> ( </symbol>
+  """ + parameterXml
+      + """<symbol> ) </symbol>
+  """
+      + subroutineBodyXml 
+      + """</subroutineDec>
+  """
+      , ts
+  let rec aux remainingTokens xml =
+    match remainingTokens with
+    | [] -> xml
+    | head::tail when head = Keyword "constructor" || head = Keyword "function" || head = Keyword "method" -> 
+      let oneDecXml, remainingTokens = doOneSubroutineDec remainingTokens
+      let xml = xml + oneDecXml
+      aux remainingTokens xml
+    | head::tail -> failwith ("Wrong Token when compiling subroutine declarations " + (string head))
+  aux tokens ""
+
+    
 let CompileClass tokens = 
  let tokens = eatIf (isSameToken (Keyword "class")) tokens
  let className, tokens = getNextTokenIf ((isSameType (Identifier "_"))) tokens
  let tokens = eatIf (isSameToken (Symbol '{')) tokens
  let classVarDecs, tokens = CompileClassVarDecs tokens
- let classSubroutineDecs, tokens = CompileSubroutineDecs tokens
+ let classSubroutineDecs = CompileSubroutineDecs tokens
  let tokens = eatIf (isSameToken (Symbol '}')) tokens
  $$"""<class>
   <keyword> class </keyword>
@@ -107,6 +161,7 @@ let CompileClass tokens =
   <symbol> } </symbol>
 </class>
 """
+
 let classTokens = [
   Keyword "class";
   Identifier "Main";
@@ -145,6 +200,52 @@ let classVarDecsTest = [
   Symbol ';'
 ]
 
+let subroutineDecsTest = [
+  Keyword "function";
+  Keyword "void";
+  Identifier "more";
+  Symbol '(';
+  Symbol ')';
+  Symbol '{';
+  Symbol '}';
+  Keyword "constructor";
+  Keyword "int";
+  Identifier "main";
+  Symbol '(';
+  Symbol ')';
+  Symbol '{';
+  Symbol '}'
+]
+let oneSubroutineTest = [
+  Keyword "function";
+  Keyword "void";
+  Identifier "more";
+  Symbol '(';
+  Symbol ')';
+  Symbol '{';
+  Symbol '}';
+]
+let curlyBracketsTest = [
+  Symbol '{';
+  Keyword "toast";
+  Symbol '{';
+  Identifier "dogloose";
+  Symbol '}'
+  Symbol ';';
+  Symbol '}';
+  Keyword "int";
+  Symbol '}'
+]
+let curlyBracketsTest2 = [
+  Symbol '{';
+  Symbol '}';
+  Keyword "pooch"
+]
+
+//printfn "%A" (doOneSubroutineDec oneSubroutineTest)
+//printfn "%A" (advanceUntilMatchingCurlyBracket curlyBracketsTest2)
+//printfn "%A" (advanceUntilMatchingCurlyBracket curlyBracketsTest)
+printfn "%A" (CompileSubroutineDecs subroutineDecsTest)
 printfn "%A" (getConsecutivePatterns (fun x -> x = (Keyword "static")) (fun x -> x = (Symbol ';')) classVarDecsTest)
 printfn "%A" (CompileClassVarDecs testTokens)
 printfn "%A" (CompileClassVarDecs classVarDecsTest)
@@ -155,7 +256,7 @@ printfn "%A" (isTypeProgramStructure [(Keyword "int")])
 printfn "%A" (isTypeProgramStructure [(Keyword "char")]) 
 printfn "%A" (isTypeProgramStructure [(Keyword "boolean")]) 
 printfn "%A" (isTypeProgramStructure [(Identifier "Point")]) 
-printfn "%A" (CompileClass classTokens)
+//printfn "%A" (CompileClass classTokens)
 printfn "%A" (eatIf (isSameToken (Symbol '}')) testTokens2)
 printfn "%A" (eatIf (isSameToken (Keyword "var")) testTokens)
 //printfn "%A" (getNextTokenIf (isSameType (Identifier "dog")) testTokens)
