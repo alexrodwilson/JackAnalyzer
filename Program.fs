812efc41f31a8628b69ea6154478881e21cf2d40
Alex Wilson <alexrodwilson@gmail.com>
2025-07-24 17:24:13 +0100

    feat: Program.fs now writes xml to files

--- Program.fs
+++ Program.fs
@@ -3,28 +3,33 @@ open Tokenizer
 open System.IO
 open CompilationEngine
 
-let tokenizeFile fn = 
-  let outFn = Path.ChangeExtension(fn, ".xml")
+let tokenizeAndXmlizeFile (fn: string) = 
+  let directoryName = Path.GetDirectoryName(fn)
+  let fileNameWithoutExtension = Path.GetFileNameWithoutExtension(fn)
+  let tokenOutFn = Path.Combine(directoryName, (fileNameWithoutExtension + "T" + ".xml"))
+  let xmlOutFn = Path.ChangeExtension (fn, ".xml")
   let text = File.ReadAllText fn
   let tokens = tokenize text
-  File.WriteAllLines(outFn, "<tokens>" :: (List.map tokenToXml  tokens) @ ["</tokens>"])
+  let xml = CompilationEngine.CompileClass tokens
+  File.WriteAllLines(tokenOutFn, "<tokens>" :: (List.map tokenToXml  tokens) @ ["</tokens>"])
+  printfn "%s written" tokenOutFn
+  File.WriteAllText (xmlOutFn, xml)
+  printfn "%s written" xmlOutFn
 
 [<EntryPoint>]
 let main args =
     //printfn "Arguments passed to function : %A" args
     // Return 0. This indicates success.
- (* let fn = args[0]
+  let fn = args[0]
   match fn with
     | fn when Directory.Exists fn -> 
       let jackFiles = Directory.GetFiles(fn, "*.jack")
       for f in jackFiles do 
-        tokenizeFile f
-        printfn "%s written" (Path.ChangeExtension (f, ".xml"))
+        tokenizeAndXmlizeFile f
     | fn when (File.Exists fn) && Path.GetExtension(fn) = ".jack" ->
-      tokenizeFile fn
-      printfn "%s written" (Path.ChangeExtension(fn, ".xml"))
-    | fn -> printfn "%s is not a valid file/directory" fn*)
-  let tokens = [(Keyword "class"); (Identifier "Main");
-    (Symbol '{'); (Symbol '}')]
-  printfn "%s" (CompileClass tokens)
+       tokenizeAndXmlizeFile fn
+    | fn -> printfn "%s is not a valid file/directory" fn
+  //let tokens = [(Keyword "class"); (Identifier "Main");
+   // (Symbol '{'); (Symbol '}')]
+ // printfn "%s" (CompileClass tokens)
   0
