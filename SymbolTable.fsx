360a2c1e70d1f3a44e7a54142ec827abb77f863c
Alex Wilson <alexrodwilson@gmail.com>
2025-08-05 17:55:12 +0100

    feat: wrote kindOf, typeOf and indexOf

--- SymbolTable.fsx
+++ SymbolTable.fsx
@@ -32,7 +32,23 @@ let define name t kind st =
   | Static | Field -> {st with ClassSymbols = (Map.add name {T = t; Kind = kind; Index = nOfKind} st.ClassSymbols)}
   | Arg | Var -> {st with SubroutineSymbols = (Map.add name {T = t; Kind = kind; Index = nOfKind} st.SubroutineSymbols)}
 
+let wipeSymbolTable st = 
+  {st with SubroutineSymbols = Map.empty}
 
+let fOf name f st = 
+  match name with
+  | _ when st.SubroutineSymbols.ContainsKey name -> (st.SubroutineSymbols[name]) |> f
+  | _ when st.ClassSymbols.ContainsKey name -> (st.ClassSymbols[name]) |> f
+  | _ -> failwith ("SymbolTable does not contain the key: " + name)
+
+let kindOf name st =
+  fOf name (fun x -> x.Kind) st
+
+let typeOf name st = 
+  fOf name (fun x -> x.T) st
+
+let indexOf name st = 
+  fOf name (fun x -> x.Index) st
 
 let st = create ()
 let st1 = define "foo" "int" Field st
@@ -50,5 +66,21 @@ printfn ""
 let st5 = define "bar3" "Point" Static st4
 printfn "%A" st5
 printfn ""
+let st6 = wipeSymbolTable st5
+printfn "%A" st6
+printfn ""
+printfn "%A" (kindOf "foo" st6)
+printfn ""
+printfn "%A" (kindOf "bar" st5)
+printfn ""
+printfn "%A" (typeOf "foo2" st5)
+printfn ""
+printfn "%A" (typeOf "bar2" st5)
+printfn ""
+printfn "%A" (indexOf "bar2" st5)
+printfn ""
+printfn "%A" (indexOf "bar3" st5)
+printfn ""
+
 
 
